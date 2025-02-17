fa02267ac27242c4cb9236762f5b6fc8bd2eb6d3
Dolaned <dylan.aird595@gmail.com>
2025-02-17 14:46:42 +0000

    Feature/remove lru cache

--- app/api/cacheApi.js
+++ app/api/cacheApi.js
@@ -1,6 +1,5 @@
 import global from "../global.js";
 import config from "../config.js";
-import LRU from "lru-cache";
 import redisCache from "../redisCache.js";
 import md5 from "md5";
 import utils from "../utils.js";
@@ -8,7 +7,8 @@ import utils from "../utils.js";
 global.cacheStats = {};
 // this value should be incremented whenever data format changes, to avoid
 // pulling old-format data from a persistent cache
-var cacheKeyVersion = "v1";
+const cacheKeyVersion = "v1";
+let redisCacheObj = null
 
 const ONE_SEC = 1000;
 const ONE_MIN = 60 * ONE_SEC;
@@ -16,128 +16,6 @@ const ONE_HR = 60 * ONE_MIN;
 const FIVE_MINUTES = 5 * ONE_MIN;
 const ONE_DAY = 24 * ONE_HR;
 const ONE_YR = 265 * ONE_DAY;
-
-
-
-function createMemoryLruCache(cacheObj, onCacheEvent) {
-	return {
-		get:function(key) {
-			return new Promise(function(resolve, reject) {
-				onCacheEvent("memory", "try", key);
-
-				var val = cacheObj.get(key);
-
-				if (val != null) {
-					onCacheEvent("memory", "hit", key);
-
-				} else {
-					onCacheEvent("memory", "miss", key);
-				}
-
-				resolve(cacheObj.get(key));
-			});
-		},
-		set:function(key, obj, maxAge) { cacheObj.set(key, obj, maxAge); }
-	}
-}
-
-async function tryCache(cacheKey, cacheObjs, index) {
-	if (index === cacheObjs.length) {
-		return null;
-	}
-
-	const result = await cacheObjs[index].get(cacheKey);
-
-	if (result !== null) {
-		return result;
-	} else {
-		return tryCache(cacheKey, cacheObjs, index + 1);
-	}
-}
-
-function createTieredCache(cacheObjs) {
-	return {
-		get: async function (key) {
-			return await tryCache(key, cacheObjs, 0);
-		},
-
-		set: function (key, obj, maxAge) {
-			for (let i = 0; i < cacheObjs.length; i++) {
-				cacheObjs[i].set(key, obj, maxAge);
-			}
-		},
-
-		append: async function (key, obj, maxAge) {
-			let result = await tryCache(key, cacheObjs, 0);
-			result = Array.isArray(result) && result.length > 0 ? [...result, obj] : [obj];
-
-			for (let i = 0; i < cacheObjs.length; i++) {
-				cacheObjs[i].set(key, result, maxAge);
-			}
-		},
-
-		prepend: async function (key, obj, maxAge) {
-			let result = await tryCache(key, cacheObjs, 0);
-			result = Array.isArray(result) && result.length > 0 ? [obj, ...result] : [obj];
-
-			for (let i = 0; i < cacheObjs.length; i++) {
-				cacheObjs[i].set(key, result, maxAge);
-			}
-		},
-		updateOrAppend: async function (cacheKey, obj, objKey, objKeyValue, maxAge) {
-			let result = await tryCache(cacheKey, cacheObjs, 0);
-			if (Array.isArray(result) && result.length > 0) {
-				const foundIndex = result.findIndex((element) => element[objKey] == objKeyValue);
-				if (foundIndex !== -1) {
-					result[foundIndex] = obj
-				} else {
-					result.push(obj);
-				}
-			} else {
-				result = [];
-				result.push(obj);
-			}
-
-			for (let i = 0; i < cacheObjs.length; i++) {
-				cacheObjs[i].set(cacheKey, result, maxAge);
-			}
-		}
-	}
-}
-
-
-
-
-var miscCaches = [];
-var blockCaches = [];
-var txCaches = [];
-var marketDataCaches = [];
-var ipAddressCaches = [];
-
-if (!config.noInmemoryRpcCache) {
-	global.cacheStats.memory = {
-		try: 0,
-		hit: 0,
-		miss: 0
-	};
-
-	var onMemoryCacheEvent = function(cacheType, eventType, cacheKey) {
-		if(!('memory' in global.cacheStats)) {
-			global.cacheStats.memory = {
-				try: 0,
-				hit: 0,
-				miss: 0
-			};
-		}
-		global.cacheStats.memory[eventType]++;
-	}
-
-	miscCaches.push(createMemoryLruCache(new LRU(2000), onMemoryCacheEvent));
-	blockCaches.push(createMemoryLruCache(new LRU(2000), onMemoryCacheEvent));
-	txCaches.push(createMemoryLruCache(new LRU(10000), onMemoryCacheEvent));
-	marketDataCaches.push(createMemoryLruCache(new LRU( 10000), onMemoryCacheEvent));
-	ipAddressCaches.push(createMemoryLruCache(new LRU( 10000), onMemoryCacheEvent));
-}
 
 if (redisCache.active) {
 	global.cacheStats.redis = {
@@ -161,32 +39,21 @@ if (redisCache.active) {
 	// md5 of the active RPC credentials serves as part of the key; this enables
 	// multiple instances of btc-rpc-explorer (eg mainnet + testnet) to share
 	// a single redis instance peacefully
-	var rpcHostPort = `${config.credentials.rpc.host}:${config.credentials.rpc.port}`;
-	var rpcCredKeyComponent = md5(JSON.stringify(config.credentials.rpc)).substring(0, 8);
-
-	var redisCacheObj = redisCache.createCache(`${cacheKeyVersion}-${rpcCredKeyComponent}`, onRedisCacheEvent);
-
-	miscCaches.push(redisCacheObj);
-	blockCaches.push(redisCacheObj);
-	txCaches.push(redisCacheObj);
-	marketDataCaches.push(redisCacheObj)
-	ipAddressCaches.push(redisCacheObj)
+	const rpcCredKeyComponent = md5(JSON.stringify(config.credentials.rpc)).substring(0, 8);
+	redisCacheObj = redisCache.createCache(`${cacheKeyVersion}-${rpcCredKeyComponent}`, onRedisCacheEvent);
+}  else {
+	console.log('The nexa explorer must have a redis cache setup to work');
+	setTimeout(process.exit(0), 3000);
 }
 
-var miscCache = createTieredCache(miscCaches);
-var blockCache = createTieredCache(blockCaches);
-var txCache = createTieredCache(txCaches);
-var marketCache = createTieredCache(marketDataCaches)
-var ipAddressCache = createTieredCache(ipAddressCaches)
-
-async function tryCacheThenCallFunction(cache, cacheKey, cacheMaxAge, functionToCall, cacheConditionFunction) {
+async function tryCacheThenCallFunction(cacheKey, cacheMaxAge, functionToCall, cacheConditionFunction) {
 	if (cacheConditionFunction == null) {
 		cacheConditionFunction = function(obj) {
 			return true;
 		};
 
 		try {
-			const cacheResult = await cache.get(cacheKey);
+			const cacheResult = await redisCacheObj.get(cacheKey);
 			if (cacheResult != null) {
 				return cacheResult;
 			}
@@ -194,7 +61,7 @@ async function tryCacheThenCallFunction(cache, cacheKey, cacheMaxAge, functionTo
 			const functionResult = await functionToCall();
 
 			if (functionResult != null && cacheConditionFunction(functionResult)) {
-				cache.set(cacheKey, functionResult, cacheMaxAge);
+				redisCacheObj.set(cacheKey, functionResult, cacheMaxAge);
 			}
 
 			return functionResult;
@@ -204,7 +71,7 @@ async function tryCacheThenCallFunction(cache, cacheKey, cacheMaxAge, functionTo
 	}
 }
 
-function tryCacheThenRpcApi(cache, cacheKey, cacheMaxAge, rpcApiFunction, cacheConditionFunction) {
+function tryCacheThenRpcApi(cacheKey, cacheMaxAge, rpcApiFunction, cacheConditionFunction) {
 
 	if (cacheConditionFunction == null) {
 		cacheConditionFunction = function(obj) {
@@ -222,7 +89,7 @@ function tryCacheThenRpcApi(cache, cacheKey, cacheMaxAge, rpcApiFunction, cacheC
 			} else {
 				rpcApiFunction().then(function(rpcResult) {
 					if (rpcResult != null && cacheConditionFunction(rpcResult)) {
-						cache.set(cacheKey, rpcResult, cacheMaxAge);
+						redisCacheObj.set(cacheKey, rpcResult, cacheMaxAge);
 					}
 
 					resolve(rpcResult);
@@ -233,7 +100,7 @@ function tryCacheThenRpcApi(cache, cacheKey, cacheMaxAge, rpcApiFunction, cacheC
 			}
 		};
 
-		cache.get(cacheKey).then(function(result) {
+		redisCacheObj.get(cacheKey).then(function(result) {
 			cacheResult = result;
 
 			finallyFunc();
@@ -253,11 +120,7 @@ export default {
 	FIVE_MINUTES,
 	ONE_DAY,
 	ONE_YR,
-	miscCache,
-	blockCache,
-	txCache,
-	marketCache,
-	ipAddressCache,
+	redisCacheObj,
 	tryCacheThenCallFunction,
 	tryCacheThenRpcApi,
 }
--- docs/server-setup.md
+++ docs/server-setup.md
@@ -2,20 +2,18 @@
 
 First install certbot on your machine by following these instructions:
 
-    https://certbot.eff.org/instructions?ws=nginx&os=ubuntufocal
+    https://certbot.eff.org/instructions?ws=nginx&os=snap
 
 Then execute this commands from terminal:
 
 ```bash
-sudo add-apt-repository ppa:certbot/certbot
 curl -sSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
-VERSION=node_10.x
-DISTRO="$(lsb_release -s -c)"
-echo "deb https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-echo "deb-src https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list
+VERSION=node_22.x
+echo "deb https://deb.nodesource.com/$VERSION nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+echo "deb-src https://deb.nodesource.com/$VERSION nodistro main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list
 sudo apt update
 sudo apt upgrade
-sudo apt install git software-properties-common nginx gcc g++ make nodejs
+sudo apt install git software-properties-common nginx gcc g++ make nodejs redis redis-server
 sudo npm install pm2 --global
 apt install python-certbot-nginx
 ```
@@ -26,11 +24,14 @@ Copy content from [./nexa-explorer.conf](./nexa-explorer.conf) into `/etc/nginx/
 ```bash
 certbot --nginx -d nexa-explorer.com #use your domain name here
 cd /home/nexa
-git clone https://github.com/nexa/explorer.git
+git clone https://gitlab.com/nexa/explorer.git
 cd /home/nexa/explorer
 npm install
 pm2 start bin/www --name "nexa-rpc-explorer"
 ```
+
+Before starting you should probably have a look at `.env-sample` to have an idea on how to configure/customise
+your explorer instance.
 
 If you want your explorer being able to show transactions with a feerate lower than 1 sat/byte you should
 configure your full nodes to accept those on its mempool. To do that if you need to add this parameter to your nexa.conf:
